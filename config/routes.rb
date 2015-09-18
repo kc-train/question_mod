cf3fb2da2218e1aa0abe36bfe9ef2b23fcb7556f
hengtangan2025 <504371515@qq.com>
2015-09-18 18:15:16 +0800

    测试

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 QuestionMod::Engine.routes.draw do
   root 'home#index'
-  resources :questions
-  resources :answers
+  resources :questions 
   resources :question_votes
+  resources :answers
   resources :answer_votes
 end
