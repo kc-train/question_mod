339a06d805ccfa5e7eb3368e39da1f697954afed
hengtangan2025 <504371515@qq.com>
2015-10-19 10:34:02 +0800

    用户不能给自己的 question 增加 answer

--- app/models/question_mod/answer.rb
+++ app/models/question_mod/answer.rb
@@ -10,7 +10,7 @@ module QuestionMod
     validates :content, :presence => true
     validates :creator, :presence => true
     validates :question, :presence => true
-    # validate :question_creator_can_not_create_answer
+    validate :question_creator_can_not_create_answer
 
     # 统计 所有 AnswerVote 的值总和
     # up +1
@@ -25,10 +25,10 @@ module QuestionMod
     has_many   :answer_votes,   :class_name => 'QuestionMod::AnswerVote'
     has_many   :comments,:class_name => 'QuestionMod::Comment', :as => :targetable
 
-    # def question_creator_can_not_create_answer
-    #   if self.creator == self.question.creator
-    #     errors.add(:base,"用户不能给自己的 question 增加 answer") 
-    #   end 
-    # end
+    def question_creator_can_not_create_answer
+      if self.creator == self.question.creator
+        errors.add(:base,"用户不能给自己的 question 增加 answer") 
+      end 
+    end
   end
 end
